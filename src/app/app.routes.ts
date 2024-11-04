4c9e948c77ba502644099e6cb4a8888ff5c80e0e
pphan <philipp.phan@adesso.de>
2024-11-04 09:51:35 +0100

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MovieDetailsComponent} from "./movie-details/movie-details.component";
 
 export const routes: Routes = [
-
+  {path: "", component: HomeComponent},
+  {path: "details/:id", component: MovieDetailsComponent}
 ];
